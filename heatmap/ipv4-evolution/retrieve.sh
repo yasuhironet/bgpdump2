3b906d5f9d24117cc7a1290548c9e8dbf36641c6
Yasuhiro Ohara <yasu@nttv6.jp>
2021-05-18 00:19:31 +0900

    filelist-init.sh, retrieve.sh

---
+++ heatmap/ipv4-evolution/retrieve.sh
@@ -0,0 +1,18 @@
+
+urlprefix="http://archive.routeviews.org/bgpdata/"
+
+years=`seq 2009 2020`
+months=`seq -w 1 12`
+days=`seq -w 1 31`
+
+for y in $years; do
+    for m in $months; do
+        if [ -f $y/$m/filelist.txt ]; then
+            for f in `cat $y/$m/filelist.txt`; do
+               echo wget -P $y/$m -nc $urlprefix/$y.$m/RIBS/$f
+               wget -P $y/$m -nc $urlprefix/$y.$m/RIBS/$f
+            done
+        fi
+    done
+done
+
